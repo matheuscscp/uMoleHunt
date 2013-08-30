369167272425fcff4814e633238b08fa0ca55da1
Matheus Pimenta <matheuscscp@gmail.com>
2013-08-30 01:52:42 -0300

    Criei um game state inicial

---
+++ src/org/unbiquitous/examples/umolehunt/StateGame.java
@@ -0,0 +1,31 @@
+package org.unbiquitous.examples.umolehunt;
+
+import org.unbiquitous.ubiengine.game.state.GameState;
+import org.unbiquitous.ubiengine.util.ComponentContainer;
+
+public class StateGame extends GameState {
+
+  public StateGame(ComponentContainer components) {
+    super(components);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void input() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void update() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void render() {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
--- src/org/unbiquitous/examples/umolehunt/UMoleHunt.java
+++ src/org/unbiquitous/examples/umolehunt/UMoleHunt.java
@@ -8,11 +8,11 @@ public final class UMoleHunt extends UosGame {
   public HashMap<String, Object> getSettings() {
     HashMap<String, Object> prop = new HashMap<String, Object>();
     
-    prop.put("window_title", "UbiForca");
-    prop.put("window_width", 1024);
-    prop.put("window_height", 768);
+    prop.put("window_title", "uMoleHunt");
+    prop.put("window_width", 1280);
+    prop.put("window_height", 720);
     
-    //prop.put("first_state", StateGame.class.getName());
+    prop.put("first_state", StateGame.class.getName());
     
     return prop;
   }
