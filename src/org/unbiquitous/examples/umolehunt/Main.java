0015dce4986fc14df944e0adf2a48eeab1fe9d91
Matheus Pimenta <matheuscscp@gmail.com>
2013-09-08 16:21:44 -0300

    Adaptando para algumas modificacoes na engine

--- src/org/unbiquitous/examples/umolehunt/Main.java
+++ src/org/unbiquitous/examples/umolehunt/Main.java
@@ -1,9 +1,9 @@
 package org.unbiquitous.examples.umolehunt;
 
-import org.unbiquitous.ubiengine.game.Starter;
+import org.unbiquitous.ubiengine.game.GameStarter;
 
 public final class Main {
   public static void main(String[] args) {
-    Starter.startGame(UMoleHunt.class.getName());
+    GameStarter.startGame(UMoleHunt.class.getName());
   }
 }
--- src/org/unbiquitous/examples/umolehunt/StateGame.java
+++ src/org/unbiquitous/examples/umolehunt/StateGame.java
@@ -1,13 +1,25 @@
 package org.unbiquitous.examples.umolehunt;
 
+import org.unbiquitous.ubiengine.game.state.GameStateArgs;
 import org.unbiquitous.ubiengine.game.state.GameState;
 import org.unbiquitous.ubiengine.util.ComponentContainer;
 
 public class StateGame extends GameState {
 
-  public StateGame(ComponentContainer components) {
-    super(components);
-    // TODO Auto-generated constructor stub
+  public StateGame(ComponentContainer components, GameStateArgs args) throws NoSuchMethodException {
+    super(components, args);
+  }
+
+  @Override
+  public void init(GameStateArgs args) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void close() {
+    // TODO Auto-generated method stub
+    
   }
 
   @Override
